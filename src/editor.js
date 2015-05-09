6ec1ad550f5f7d0637d524354f17d3848c3bab7c
Julian Hollmann <julian.hollmann@gmail.com>
2015-05-09 22:28:24 +0200

    Switch to dark editor theme

--- src/editor.js
+++ src/editor.js
@@ -5,6 +5,7 @@ export default class Editor {
   constructor(domNodeId) {
     this.ace = new Ace();
     this.ace.setDomNodeId(domNodeId);
+    this.ace._editor.setTheme('ace/theme/twilight');
   }
 
   setContent(content) {
